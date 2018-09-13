a9568c55170db6b65523fd3e00d0f9653a80b702
fedejimenez <fedej_mdp@hotmail.com>
2018-09-13 13:17:45 +0800

    Add general improvements (UI/UE)

--- app/controllers/question_votes_controller.rb
+++ app/controllers/question_votes_controller.rb
@@ -4,25 +4,22 @@ post '/question/:id/upvote' do
 		if vote # Check if question has been voted by user
 			if vote.vote == 1 # Check if vote is an upvote
 				@message = "You can only upvote once per question"
-				@question = Question.find(params[:id])
-				erb :'questions/show'
 			else # If vote is a downvote, change vote to upvote
 				@message = "Your vote has been changed to upvote"
 				vote.update(vote: 1)
-				@question = Question.find(params[:id])
-				erb :'questions/show'
 			end
 		else # If question has not been voted, create new vote
 			@message = "You have upvoted for this question"
 			current_user.question_votes.create(question_id: params[:id], vote: 1)
-			@question = Question.find(params[:id])
-			erb :'questions/show'
 		end
+		@question = Question.find(params[:id])
+		erb :'questions/show'
 	else
 		@message = "Please login to vote"
 		erb :'sessions/new'
 	end
-	return @message
+	@param = [@question.count_upvotes.to_s,",", @question.count_downvotes.to_s,",", @message]
+	return @param
 end
 
 post '/question/:id/downvote' do 
@@ -31,23 +28,20 @@ post '/question/:id/downvote' do
 		if vote # Check if question has been voted by user
 			if vote.vote == 0 # Check if vote is an downvote
 				@message = "You can only downvote once per question"
-				@question = Question.find(params[:id])
-				erb :'questions/show'
 			else # If vote is a upvote, change vote to downvote
 				@message = "Your vote has been changed to downvote"
 				vote.update(vote: 0)
-				@question = Question.find(params[:id])
-				erb :'questions/show'
 			end
 		else # If question has not been voted, create new vote
 			@message = "You have downvoted for this question"
 			current_user.question_votes.create(question_id: params[:id], vote: 0)
-			@question = Question.find(params[:id])
-			erb :'questions/show'
 		end
+		@question = Question.find(params[:id])
+		erb :'questions/show'
 	else
 		@message = "Please login to vote"
 		erb :'sessions/new'
 	end
-	return @message
+	@param = [@question.count_upvotes.to_s,",", @question.count_downvotes.to_s,",", @message]
+	return @param
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,40 +1,23 @@
-function upvote(id){
-	let url = "/question/"+id+"/upvote";
+function vote(id, type){
+	let url = "/question/"+id+"/"+type;
 	// 1. Call the AJAX function
 	let request = $.ajax({
 	  method: "POST", // Set request type
 	  url: url, // Set URL for AJAX to point to
 	  // data: {"key_name": "value"} // Specify data to post
+	  cache: false
 	});
 	
 	request.done(function(response) {
 	  // 2. What to do after receiving a response from the server	
-	  $('#message-vote').html(response);
-	  // $("#upvote").load(location.href + " #upvote");
-	  $("#upvote-count").load(location.href+" #upvote-count>*"," ");
-	  $("#downvote-count").load(location.href+" #downvote-count>*"," ");
-      
-	});
-
-	request.fail(function() {
-    alert( "error" );
-  	});
-};
-
-function downvote(id){
-	let url = "/question/"+id+"/downvote";
-	// 1. Call the AJAX function
-	let request = $.ajax({
-	  method: "POST", // Set request type
-	  url: url, // Set URL for AJAX to point to
-	  // data: {"key_name": "value"} // Specify data to post
-	});
-	
-	request.done(function(response) {
-	  // 2. What to do after receiving a response from the server	
-	  $('#message-vote').html(response);
-	  $("#upvote-count").load(location.href+" #upvote-count>*"," ");
-	  $("#downvote-count").load(location.href+" #downvote-count>*"," ");
+	   let array = response.split(",");
+	   $().html(array[2]);
+	   let message = "#message-vote-"+id.toString()
+	   let upvote = "#upvote-"+id.toString();
+	   let downvote = "#downvote-"+id.toString();
+	   $(message).html(array[2]);
+	   $(upvote).replaceWith("<span id=upvote-"+id+"> | "+ array[0] + "</span>") ;
+	   $(downvote).replaceWith("<span id=downvote-"+id+"> | "+ array[1] + "</span>") ;
 	});
 
 	request.fail(function() {
